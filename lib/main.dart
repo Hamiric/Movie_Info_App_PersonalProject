89e44311e4d0c9b600708e832c2a14e51ef46198
Hamiric <do9870@naver.com>
2024-12-19 20:06:48 +0900

    [Feat] : go_router 설치 및 홈페이지 라우트 설정 #1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movie_info_app_personalproject/presentation/app/route.dart';
 import 'package:movie_info_app_personalproject/presentation/theme/theme.dart';
 
 void main() {
@@ -16,12 +17,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       themeMode: ThemeMode.dark,
       theme: lightTheme,
       darkTheme: darkTheme,
-      // home: 
+      
+      routerConfig: router,
     );
   }
 }
--- lib/presentation/ui/homepage/homepage.dart
+++ lib/presentation/ui/homepage/homepage.dart
@@ -8,18 +8,21 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ReponsiveView(
       mobile: Scaffold(
-        body: ListView(
-          scrollDirection: Axis.vertical,
-          children: [
-            // 가장 인기있는 - text
-            // 영화 포스터 - Image
-            // 현재 상영중 - text
-            // 스크롤(가로) 가능한 영화 포스터들 - Row
-            // 인기순 - text
-            // 스크롤(가로) 가능한 영화 포스터들 - Row(Stack)
-            // 평좀 높은순 - text
-            // 스크롤(가로) 가능한 영화 포스터들 - Row
-          ],
+        body: SafeArea(
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              Text('가장 인기있는'),
+              // 가장 인기있는 - text
+              // 영화 포스터 - Image
+              // 현재 상영중 - text
+              // 스크롤(가로) 가능한 영화 포스터들 - Row
+              // 인기순 - text
+              // 스크롤(가로) 가능한 영화 포스터들 - Row(Stack)
+              // 평좀 높은순 - text
+              // 스크롤(가로) 가능한 영화 포스터들 - Row
+            ],
+          ),
         ),
       ),
     );
