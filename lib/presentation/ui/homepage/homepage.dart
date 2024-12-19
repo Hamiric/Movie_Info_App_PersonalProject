07bfcdfbdc824fc9639326fbfe04971a5260dd96
Hamiric <do9870@naver.com>
2024-12-19 19:51:25 +0900

    [Feat] : 반응형 UI 를 위한 ReponsiveView 설정 #1

---
+++ lib/presentation/ui/homepage/homepage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:movie_info_app_personalproject/presentation/widgets/reponsiveview.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ReponsiveView(
+      mobile: Scaffold(
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            // 가장 인기있는 - text
+            // 영화 포스터 - Image
+            // 현재 상영중 - text
+            // 스크롤(가로) 가능한 영화 포스터들 - Row
+            // 인기순 - text
+            // 스크롤(가로) 가능한 영화 포스터들 - Row(Stack)
+            // 평좀 높은순 - text
+            // 스크롤(가로) 가능한 영화 포스터들 - Row
+          ],
+        ),
+      ),
+    );
+  }
+}
