eee6802c874e26a40986ff3cb381e961fb6cc97c
Hamiric <do9870@naver.com>
2024-12-24 17:27:29 +0900

    [Feat] : 디테일페이지 route 연결 #6

---
+++ lib/presentation/ui/detailpage/detailpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Detailpage extends StatelessWidget {
+  const Detailpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: ListView(
+        children: [
+          
+        ],
+      )),
+    );
+  }
+}
--- lib/presentation/ui/homepage/widgets/current_movie.dart
+++ lib/presentation/ui/homepage/widgets/current_movie.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CurrentMovie extends StatelessWidget {
   const CurrentMovie({super.key});
@@ -10,12 +11,17 @@ class CurrentMovie extends StatelessWidget {
       child: Row(
         children: List.generate(20, (index) => Padding(
           padding: const EdgeInsets.only(right: 8),
-          child: Container(
-            height: 180,
-            width: 120,
-            decoration: BoxDecoration(
-              color: Colors.amber,
-              borderRadius: BorderRadius.circular(8)
+          child: GestureDetector(
+            onTap: (){
+              context.go('/post/');
+            },
+            child: Container(
+              height: 180,
+              width: 120,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(8)
+              ),
             ),
           ),
         )),
