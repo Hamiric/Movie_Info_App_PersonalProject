a1918301bd3b7db611f4a82037b968c1b7b68860
Hamiric <do9870@naver.com>
2024-12-24 20:23:50 +0900

    [Feat] : 페이지 전환 애니메이션 #8

--- lib/presentation/app/route.dart
+++ lib/presentation/app/route.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:movie_info_app_personalproject/presentation/ui/detailpage/detailpage.dart';
 import 'package:movie_info_app_personalproject/presentation/ui/homepage/homepage.dart';
@@ -10,7 +11,7 @@ final router = GoRouter(
       routes: [
         GoRoute(
           path: 'post',
-          builder: (context, state) => const Detailpage(),
+          pageBuilder: (context, state) => slideTransition(const Detailpage()),
         )
       ],
     ),
@@ -23,3 +24,21 @@ final router = GoRouter(
   }
   */
 );
+
+
+/// 페이지 슬라이드 효과
+CustomTransitionPage slideTransition(Widget page) {
+  return CustomTransitionPage(
+    child: page,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+        SlideTransition(
+      position: animation.drive(
+        Tween<Offset>(
+          begin: const Offset(1.25, 0),
+          end: Offset.zero,
+        ).chain(CurveTween(curve: Curves.easeIn)),
+      ),
+      child: child,
+    ),
+  );
+}
